1f4d15d89e28447be9824c3396f91708be79a099
Arshad M <arshadpkm@gmail.com>
2017-06-09 19:55:25 +0530

    index file edited

--- public/index.php
+++ public/index.php
@@ -1,21 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once '../app/init.php';
 
 $app = new App();
-
-/**
-$_no = $_GET['number'];
-
-echo '<pre>';
-
-var_dump($_GET);
-
-echo $_no;
-
-echo 'ok';
-*/
-?>
